c344763d82a3fb04317c04c662dafd4f4159fa39
zhigunovep <zhigunovep@intersvyaz.net>
2016-05-23 22:23:06 +0500

    initial

---
+++ app/src/main/java/com/example/ai/gamelife/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ai.gamelife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

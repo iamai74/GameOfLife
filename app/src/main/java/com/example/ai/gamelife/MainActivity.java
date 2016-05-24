62275e5ee619e51ae6b5c7141e1d831ba764eae2
zhigunovep <zhigunovep@intersvyaz.net>
2016-05-24 22:11:33 +0500

    toggle cell color change data in main array

--- app/src/main/java/com/example/ai/gamelife/MainActivity.java
+++ app/src/main/java/com/example/ai/gamelife/MainActivity.java
@@ -1,15 +1,53 @@
 package com.example.ai.gamelife;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Integer[] data = {
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,0,0
+    };
+
+    GridView gvMain;
+    ArrayAdapter<Integer> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<Integer>(this, R.layout.item, R.id.tvText, data);
+        gvMain = (GridView) findViewById(R.id.sandBox);
+        gvMain.setAdapter(adapter);
+
+        gvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                View cell = gvMain.getChildAt(i);
+                if (data[i]==0) {
+                    data[i] = 1;
+                    cell.setBackgroundColor(Color.BLACK);
+                }else {
+                    data[i]=0;
+                    cell.setBackgroundColor(Color.WHITE);
+                }
+
+            }
+        });
     }
 }
